9b200b7c295917307e3f229fdb2133ecfec0f1a8
SakuraSher <sakurasherlocked@gmail.com>
2025-06-10 22:19:39 +0530

    app code

--- app-test.js
+++ app-test.js
@@ -1,64 +1,190 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./app');
-const mongoose = require('mongoose');
-
-chai.use(chaiHttp);
-const expect = chai.expect;
-
-describe('Solar System API', function() {
-  this.timeout(10000); // Increase timeout for all tests
-
-  before(async () => {
-    // Skip if already connected
-    if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
-    }
-    // Clear test database
-    await mongoose.connection.db.dropDatabase();
-  });
-
-  after(async () => {
-    await mongoose.disconnect();
-  });
+let mongoose = require("mongoose");
+let server = require("./app");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 
 
-  describe('GET /', () => {
-    it('should serve index.html', (done) => {
-      chai.request(app)
-        .get('/')
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-          done();
+// Assertion 
+chai.should();
+chai.use(chaiHttp); 
+
+describe('Planets API Suite', () => {
+
+    describe('Fetching Planet Details', () => {
+        it('it should fetch a planet named Mercury', (done) => {
+            let payload = {
+                id: 1
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(1);
+                    res.body.should.have.property('name').eql('Mercury');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Venus', (done) => {
+            let payload = {
+                id: 2
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(2);
+                    res.body.should.have.property('name').eql('Venus');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Earth', (done) => {
+            let payload = {
+                id: 3
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(3);
+                    res.body.should.have.property('name').eql('Earth');
+                done();
+              });
+        });
+        it('it should fetch a planet named Mars', (done) => {
+            let payload = {
+                id: 4
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(4);
+                    res.body.should.have.property('name').eql('Mars');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Jupiter', (done) => {
+            let payload = {
+                id: 5
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(5);
+                    res.body.should.have.property('name').eql('Jupiter');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Satrun', (done) => {
+            let payload = {
+                id: 6
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(6);
+                    res.body.should.have.property('name').eql('Saturn');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Uranus', (done) => {
+            let payload = {
+                id: 7
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(7);
+                    res.body.should.have.property('name').eql('Uranus');
+                done();
+              });
+        });
+
+        it('it should fetch a planet named Neptune', (done) => {
+            let payload = {
+                id: 8
+            }
+          chai.request(server)
+              .post('/planet')
+              .send(payload)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(8);
+                    res.body.should.have.property('name').eql('Neptune');
+                done();
+              });
+        });
+
+        // it('it should fetch a planet named Pluto', (done) => {
+        //     let payload = {
+        //         id: 9
+        //     }
+        //   chai.request(server)
+        //       .post('/planet')
+        //       .send(payload)
+        //       .end((err, res) => {
+        //             res.should.have.status(200);
+        //             res.body.should.have.property('id').eql(9);
+        //             res.body.should.have.property('name').eql('Sun');
+        //         done();
+        //       });
+        // });
+
+
+    });        
+});
+
+//Use below test case to achieve coverage
+describe('Testing Other Endpoints', () => {
+
+    describe('it should fetch OS Details', () => {
+        it('it should fetch OS details', (done) => {
+          chai.request(server)
+              .get('/os')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                done();
+              });
         });
     });
-  });
 
-  describe('POST /planet', () => {
-    it('should return planet data for valid ID', (done) => {
-      chai.request(app)
-        .post('/planet')
-        .send({ id: 1 })
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.have.property('name');
-          done();
+    describe('it should fetch Live Status', () => {
+        it('it checks Liveness endpoint', (done) => {
+          chai.request(server)
+              .get('/live')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('live');
+                done();
+              });
         });
     });
 
-    it('should handle invalid planet IDs', (done) => {
-      chai.request(app)
-        .post('/planet')
-        .send({ id: 999 })
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.text).to.equal('Error in Planet Data');
-          done();
+    describe('it should fetch Ready Status', () => {
+        it('it checks Readiness endpoint', (done) => {
+          chai.request(server)
+              .get('/ready')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('ready');
+                done();
+              });
         });
     });
-  });
+
 });
